a811055ccfe104ccd22c68cb9de1a9577f522f90
rafi.hariman <231232609@uii.ac.id>
2025-09-30 16:26:29 +0700

    chore: update API base URL to use cloud API and adjust proxy configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 import { Environment } from './environment.interface';
 
+const API_BASE_URL = 'https://cloud-api.sena-digital.com/api';
+
 export const environment: Environment = {
   production: true,
-  apiBaseUrl: 'https://www.sena-digital.com/api'
+  apiBaseUrl: API_BASE_URL
 };
